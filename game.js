b331cdaf7ef4fdf7aa94f12fc1de34deb405b156
Ariel Cameron <ariel.cameron@gmail.com>
2024-06-03 22:23:30 -0700

    Saving before editing for style

--- game.js
+++ game.js
@@ -25,7 +25,6 @@ let computerChoice = computerChoiceWord;
 console.log("Computer's Choice: ", computerChoiceWord);
 
 // Function that takes the user choice (paper, scissors or rock) and returns it
-
 function getHumanChoice() {
     let humanChoice = prompt("Enter your choice: Paper, Scissors or Rock").toUpperCase();
     console.log("User's Choice: ", humanChoice);
@@ -35,7 +34,6 @@ function getHumanChoice() {
 let userChoice = getHumanChoice();
 
 // Determine Human's game outcome
-
 function humanOutcome(userChoice, computerChoice) {
     if (userChoice === computerChoice) {
         return "It's a tie";
@@ -48,10 +46,12 @@ function humanOutcome(userChoice, computerChoice) {
     } else {
        return "You lose!";
     }
-}         
+}
+
+let outcome = humanOutcome(userChoice, computerChoice);
 
 // Prints out an outcome message, based on Human outcome (win, lose or tie with computer choice)
-console.log(humanOutcome(userChoice, computerChoice));
+console.log(outcome);
 
 // Declare payers scores
 let humanScore = 0;
@@ -65,7 +65,7 @@ function incrementComputerScore () {
     computerScore++;
 }
 
-// Function to play round and update score based on game outcome
+// Function to play round and increment score based on game outcome
 function playRound (userChoice, computerChoice) {
     let outcome = humanOutcome(userChoice, computerChoice);
     console.log("Outcome: ", outcome); //debugging line
@@ -85,6 +85,44 @@ function playRound (userChoice, computerChoice) {
 let playerChoice = userChoice;
 let computersChoice = computerChoice;
 
-playRound(playerChoice, computersChoice);
 
-// Determine if user is winner or loser
+// Logic for playing full game (consisting of 5 rounds)
+/* function playGame() {
+    let rounds = 0;
+    while (rounds < 5 && humanScore < 3 && computerScore < 3) {
+        let computerChoiceNumber = getComputerChoice();
+        let computerChoiceWord = numberToWord(computerChoiceNumber);
+        console.log("Computer's Choice: ", computerChoiceWord);
+
+        let userChoice = getHumanChoice();
+
+        playRound(userChoice, computerChoiceWord);
+
+        rounds++;
+    }
+
+    if(humanScore === 3) {
+        console.log("You win the game!");
+    } else if (computerScore === 3) {
+        console.log("You lose the game!");
+    } else {
+        console.log("Game over. It's a tie.")
+    }
+}
+// Make function called playGame
+// Inside playGame: prompt user for paper, scissors or rock (humanChoice)
+// get random choice from computer (computerChoice)
+// compare userChoice with computerChoice to determine winner
+// print out winner and increment score
+// repeat the prompt from human
+// play until someone reaches 3 wins (in a max of 5 rounds)
+// game ends and prints the name of winner (you win or you lose in regards to human)
+
+/* playRound takes userChoice and computerChoice and determines winner 
+as "outcome" using the humanOutcome function
+
+to play a round 5 times, I need to call the playRound function
+5 times but also call getHumanChoice and getComputerChoice (computerChoiceWord)
+to repromt a new game. 
+
+*/
